b5689f7e9af7c36882c4ed72755fd385d4a537c4
Maximilien Zaleski <maximilienzaleski@yahoo.com>
2022-12-16 19:40:52 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,157 @@
+# useSendCode
+
+A React (NextJS-oriented) hook that facilitates operations such as sending one-time codes.
+
+## Table of Contents
+
+- [useSendCode](#usesendcode)
+  - [Table of Contents](#table-of-contents)
+  - [Features](#features)
+  - [Motivation](#motivation)
+    - [Caveat](#caveat)
+  - [Installation](#installation)
+  - [Usage](#usage)
+    - [Configuration](#configuration)
+    - [Properties](#properties)
+  - [Understanding the hook's behaviour](#understanding-the-hooks-behaviour)
+    - [When a user clicks on the CTA](#when-a-user-clicks-on-the-cta)
+    - [When the cooldown is active, but the user refreshes the page](#when-the-cooldown-is-active-but-the-user-refreshes-the-page)
+    - [When the cooldown period is over](#when-the-cooldown-period-is-over)
+  - [TypeScript Support](#typescript-support)
+  - [License](#license)
+
+---
+
+## Features
+
+- Handle spam-sensitive operations such as one-time codes
+- Session tracking through server-only cookie
+- Session recovery on reload
+- Component (button) props that handle every aspect of the user-facing CTA (label, disabled, loading, etc)
+
+## Motivation
+
+Low-cost solution to spam-sensitive operations. This solution omits the use of a cache which might not always be available in early-stage applications.
+
+### Caveat
+
+This solution was designed around the ability to retrieve server-only cookies with relative ease, e.g. NextJS' `getServerSideProps`. A vanilla React application would not be suitable for this solution.
+
+## Installation
+
+Install through your package manager of choice (npm, yarn, etc)
+
+```
+npm -i @mzaleski/use-send-code
+```
+
+```
+yarn add @mzaleski/use-send-code
+```
+
+## Usage
+
+Example usage with NextJS' `getServerSideProps`:
+
+```tsx
+import { useSendCode } from '@mzaleski/use-send-code';
+
+async function worker(iamIdentifier: string): Promise<void> { ... }
+
+function Page(props) {
+  const { sendCode, status, buttonProps } = useSendCode(worker, {
+    iamIdentifier: 'some-id',
+    lastCodeIdentifier: props.cookiePayload?.iamIdentifier,
+    lastCodeSendAt: props.cookiePayload?.sentAt,
+    callOnMount: true,
+    cooldownPeriod: 2 * 60,
+    buttonPropsActiveLabel: 'Recover my account',
+    buttonPropsLoadingPropName: 'isLoading',
+    debugStatements: process.env.NODE_ENV === 'development',
+    sessionClearHandler: async () => { ... },
+    sessionPersistHandler: async (iamIdentifier) => { ... },
+  });
+
+  const sendRecoveryCode = async () => {
+    try {
+      // You may wish to include additional logic here
+      await sendCode();
+    } catch (err: any) {
+      // handle error
+      console.error(err);
+    }
+  };
+
+  return (
+    <button {...buttonProps} onClick={sendRecoveryCode}>
+      {buttonProps.label}
+    </button>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      cookiePayload: /** retrieve server-only cookie and deserialise it */,
+    },
+  };
+}
+```
+
+An interactive example can be found on [Stackblitz](https://stackblitz.com/edit/nextjs-qlxy3n?file=README.md).
+
+### Configuration
+
+The hook's configuration is done through the `opts` object which has the following properties:
+
+| Name | Type | Description | Default |
+| --- | --- | --- | --- |
+iamIdentifier | String | The current user's unique identifier | [required] |
+sessionClearHandler | Function | A function responsible for clearing the server-only cookie | [required] |
+sessionPersistHandler | Function | - A function responsible for creating the server-only cookie; it is given the `iamIdentifier` as parameter - The signature encourages the return of a server timestamp | [required] |
+lastCodeIdentifier? | String | [retrieved from server-only cookie] the last code's `iamIdentifier` | `undefined` |
+lastCodeSendAt? | String | [retrieved from server-only cookie] when the last code was sent; it will be parsed by the Javascript `Date` class | `undefined` |
+callOnMount? | Boolean | Whether to call the worker on component mount | `false` |
+cooldownPeriod? | Number | The cooldown period in seconds | `300` (5 minutes) |
+buttonPropsActiveLabel? | String | The button's label when a new code is available | `"Send me a new code"` |
+buttonPropsLoadingPropName? | String | [Custom component support] specify the `loading` boolean property name on a custom button component | `"loading"` |
+debugStatements? | Boolean | Whether to log debug statements (requires development mode) | `false` |
+
+### Properties
+
+The hook returns an object with the following properties:
+
+| Name | Type | Description |
+| --- | --- | --- |
+sendCode | Function | The function responsible for calling the worker and updating the internal state |
+status | String | The current status of the hook; it can be one of the following: `"READY"`, `"COOLDOWN"`, `"SENDING"`, `"RESTORING"` |
+buttonProps | Object | The props that should be passed to the button component; affected by `buttonPropsActiveLabel`, `buttonPropsLoadingPropName` |
+
+## Understanding the hook's behaviour 
+
+### When a user clicks on the CTA
+
+The hook will...
+
+1. Call your worker and perform the operation
+2. Call `sessionPersistHandler` with the given `iamIdentifier` to create a server-only cookie
+3. Update its internal state as well the button's props in order to reflect the change 
+
+### When the cooldown is active, but the user refreshes the page
+
+The hook will restore the previous state by looking at `lastCodeIdentifier` and `lastCodeSendAt` from the server-only cookie.
+   
+### When the cooldown period is over
+
+The hook will...
+
+1. Call `sessionClearHandler` to clear the server-only cookie
+2. Update its internal state as well the button's props in order to reflect the change
+
+## TypeScript Support 
+
+You will find a collection of typings bundled with the package.
+
+## License
+
+[MIT License](LICENSE) (c) 2022 Maximilien Zaleski
---
+++ src/index.test.ts
@@ -0,0 +1,278 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { EUseSendCodeStatus } from './index';
+import { useSendCode } from './';
+
+const commonProps = {
+  iamIdentifier: 'this-is-a-test-iam',
+  freezeTimer: true,
+
+  sessionClearHandler: async () => {},
+  sessionPersistHandler: async () => {},
+};
+const fn = async () => {};
+
+describe('init', () => {
+  const fn = jest.fn();
+  const opts = {
+    ...commonProps,
+    sessionClearHandler: jest.fn(),
+    sessionPersistHandler: jest.fn(),
+  };
+  const { result } = renderHook(() => useSendCode(fn, opts));
+
+  it('should set status to "READY"', () => {
+    expect(result.current.status).toBe(EUseSendCodeStatus.READY);
+  });
+
+  it('should unset `buttonProps.disabled`', () => {
+    expect(result.current.buttonProps.disabled).toBe(false);
+  });
+
+  it('should unset `buttonProps.loading`', () => {
+    expect(result.current.buttonProps.loading).toBeFalsy();
+  });
+
+  it('should set `buttonProps.children` to default', () => {
+    expect(result.current.buttonProps.children).toBe('Send me a new code');
+  });
+
+  it('should set `buttonProps.children` to `buttonPropsActiveLabel`', () => {
+    const { result } = renderHook(() =>
+      useSendCode(fn, {
+        ...commonProps,
+        buttonPropsActiveLabel: 'test',
+      })
+    );
+    expect(result.current.buttonProps.children).toBe('test');
+  });
+
+  it('should not call any function', () => {
+    expect(fn).not.toHaveBeenCalled();
+    expect(opts.sessionClearHandler).not.toHaveBeenCalled();
+    expect(opts.sessionPersistHandler).not.toHaveBeenCalled();
+  });
+
+  it('should log debug statements if `debugStatements` is set', () => {
+    console.table = jest.fn();
+    console.info = jest.fn();
+    renderHook(() =>
+      useSendCode(fn, {
+        ...commonProps,
+        debugStatements: true,
+      })
+    );
+
+    expect(console.info).toHaveBeenCalledWith(
+      '[useSendCode] `opts.debugStatements` is enabled'
+    );
+    expect(console.table).toHaveBeenCalledWith({
+      status: EUseSendCodeStatus.RESTORING,
+      iamIdentifier: commonProps.iamIdentifier,
+      isFrozen: true,
+      initialTime: 300,
+    });
+  });
+});
+
+describe('restoring existing session', () => {
+  it('should tick down', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, {
+        ...commonProps,
+        cooldownPeriod: 3,
+        lastCodeIdentifier: commonProps.iamIdentifier,
+        lastCodeSentAt: new Date().toISOString(),
+      })
+    );
+    for (let i = 3; i > 0; i--) {
+      expect(result.current.buttonProps.children).toBe(
+        'Next code available in 00:' + (i < 10 ? '0' + i : i)
+      );
+      await waitForNextUpdate();
+    }
+  });
+
+  it('should remove remnants of expired session', async () => {
+    const opts = {
+      ...commonProps,
+      cooldownPeriod: 1,
+      lastCodeIdentifier: commonProps.iamIdentifier,
+      lastCodeSentAt: new Date(new Date().getTime() - 5000).toISOString(),
+      sessionClearHandler: jest.fn(),
+      sessionPersistHandler: jest.fn(),
+    };
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, opts)
+    );
+    await waitForNextUpdate();
+
+    expect(result.current.status).toBe(EUseSendCodeStatus.READY);
+    expect(opts.sessionClearHandler).toBeCalledTimes(1);
+    expect(opts.sessionPersistHandler).toBeCalledTimes(0);
+  });
+
+  it('should remove remnants of corrupted session', async () => {
+    const opts = {
+      ...commonProps,
+      lastCodeIdentifier: 'corrupted-iam',
+      lastCodeSentAt: new Date().toISOString(),
+      sessionClearHandler: jest.fn(),
+    };
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, opts)
+    );
+    await waitForNextUpdate();
+
+    expect(result.current.status).toBe(EUseSendCodeStatus.READY);
+    expect(opts.sessionClearHandler).toBeCalledTimes(1);
+  });
+
+  it('should throw error on invalid `lastCodeSendAt`', () => {
+    renderHook(() => {
+      try {
+        useSendCode(fn, {
+          ...commonProps,
+          lastCodeIdentifier: commonProps.iamIdentifier,
+          lastCodeSentAt: 'invalid-date',
+        });
+        expect(true).toBe(false);
+      } catch (err: unknown) {
+        expect((err as Error).message).toBe(
+          '[useSendCode] Invalid `lastCodeSentAt` value, expected parse-able date'
+        );
+      }
+    });
+  });
+});
+
+describe('cooldown', () => {
+  const { result } = renderHook(() =>
+    useSendCode(fn, {
+      ...commonProps,
+      lastCodeIdentifier: commonProps.iamIdentifier,
+      lastCodeSentAt: new Date().toISOString(),
+    })
+  );
+
+  it('should set status to "COOLDOWN"', () => {
+    expect(result.current.status).toBe(EUseSendCodeStatus.COOLDOWN);
+  });
+
+  it('should unset `buttonProps.disabled`', () => {
+    expect(result.current.buttonProps.disabled).toBe(true);
+  });
+
+  it('should unset `buttonProps.loading`', () => {
+    expect(result.current.buttonProps.loading).toBeFalsy();
+  });
+
+  it('should set `buttonProps.children`', () => {
+    expect(result.current.buttonProps.children).toBe(
+      'Next code available in 05:00'
+    );
+  });
+
+  it('should set status to "COOLDOWN" after `callOnMount`', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, {
+        ...commonProps,
+        callOnMount: true,
+      })
+    );
+    await waitForNextUpdate();
+    expect(result.current.status).toBe(EUseSendCodeStatus.COOLDOWN);
+  });
+
+  it('should set status to "READY" on completion', async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, { ...commonProps, cooldownPeriod: 1 })
+    );
+    await act(async () => result.current.sendCode() as Promise<void>);
+    await waitForNextUpdate();
+    expect(result.current.status).toBe(EUseSendCodeStatus.READY);
+  });
+});
+
+describe('sending code', () => {
+  // Ignore verbose error about async `act` calls as the point is to test during the async call.
+  console.error = jest.fn();
+
+  it('should set status to "SENDING"', async () => {
+    const fn = jest.fn();
+    const { result } = renderHook(() => useSendCode(fn, commonProps));
+    act(() => {
+      result.current.sendCode();
+    });
+
+    expect(fn).toBeCalledTimes(1);
+    expect(result.current.status).toBe(EUseSendCodeStatus.SENDING);
+  });
+
+  it('should mutate button props', async () => {
+    const { result } = renderHook(() => useSendCode(fn, commonProps));
+    act(() => {
+      result.current.sendCode();
+    });
+
+    expect(result.current.buttonProps.disabled).toBe(true);
+    expect(result.current.buttonProps.loading).toBe(true);
+    expect(result.current.buttonProps.children).toBe('Sending code...');
+  });
+
+  it('should set status to "COOLDOWN" and call session handlers', async () => {
+    const opts = {
+      ...commonProps,
+      sessionClearHandler: jest.fn(),
+      sessionPersistHandler: jest.fn(),
+    };
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, opts)
+    );
+    await act(async () => result.current.sendCode() as Promise<void>);
+    await waitForNextUpdate();
+
+    expect(result.current.status).toBe(EUseSendCodeStatus.COOLDOWN);
+    expect(opts.sessionClearHandler).toBeCalledTimes(0);
+    expect(opts.sessionPersistHandler).toBeCalledTimes(1);
+  });
+
+  it('should call worker when `callOnMount` is set', async () => {
+    const fn = jest.fn();
+    const { waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, {
+        ...commonProps,
+        callOnMount: true,
+      })
+    );
+    await waitForNextUpdate();
+    expect(fn).toBeCalledTimes(1);
+  });
+
+  it('should re-throw `sendCode` error', async () => {
+    const fn = jest.fn(() => {
+      throw new Error('Test');
+    });
+    const { result } = renderHook(() => useSendCode(fn, commonProps));
+    await expect(
+      act(async () => result.current.sendCode() as Promise<void>)
+    ).rejects.toThrow('Test');
+  });
+});
+
+describe('timer', () => {
+  it('should reset when completed', async () => {
+    const opts = {
+      ...commonProps,
+      cooldownPeriod: 1,
+      sessionClearHandler: jest.fn(),
+    };
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useSendCode(fn, opts)
+    );
+    await act(async () => result.current.sendCode() as Promise<void>);
+    await waitForNextUpdate();
+
+    expect(result.current.status).toBe(EUseSendCodeStatus.READY);
+    expect(opts.sessionClearHandler).toBeCalledTimes(1);
+  });
+});
